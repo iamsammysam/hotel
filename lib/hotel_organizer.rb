2a41836ea27a9a86bbe30dfa18147d0999a06d9b
Samantha Coll <samcoll@Samanthas-MacBook-Pro.local>
2019-09-07 13:58:40 -0700

    created list_rooms, make_reservation and tests

--- lib/hotel_organizer.rb
+++ lib/hotel_organizer.rb
@@ -1,5 +1,3 @@
-require "pry"
-
 module Hotel
   class HotelOrganizer
     attr_reader :reservations, :rooms
@@ -11,6 +9,28 @@ module Hotel
       20.times.each do |i|
         @rooms << Hotel::Room.new(i + 1)
       end
+    end
+    
+    def list_rooms
+      @rooms
+    end
+    
+    def make_reservation(reservation_id, room_number, start_date, end_date)  
+      reservation_id = (reservations.length + 1)
+      room = find_room(room_number)
+      
+      reservation = Hotel::Reservation.new(reservation_id, room, start_date, end_date)
+      return reservation
+    end
+    
+    def find_room(room_number)
+      rooms.each do |room|
+        if room.room_number == room_number
+          return room
+        end
+      end
+      
+      return nil
     end
   end
 end 
--- test/hotel_organizer_test.rb
+++ test/hotel_organizer_test.rb
@@ -14,9 +14,30 @@ describe "HotelOrganizer Class" do
       expect(@hotel_organizer.rooms).must_be_kind_of Array
       expect(@hotel_organizer.reservations).must_be_kind_of Array
     end
+  end 
+  
+  describe "method list_rooms" do 
+    it "method returns an array" do
+      rooms = @hotel_organizer.list_rooms
+      expect(rooms).must_be_kind_of Array
+    end
     
-    it "checks that rooms contains 20 rooms" do
+    it "rooms array contains 20 rooms" do
       expect(@hotel_organizer.rooms.length).must_equal 20
+    end
+  end
+  
+  describe "method make_reservation" do 
+    before do
+      @reservation = @hotel_organizer.make_reservation(1, 18, Date.new(2018,01,01), Date.new(2018,01,06))
+    end
+    
+    it "returns an instance of Reservation" do
+      expect(@reservation).must_be_kind_of Hotel::Reservation
+    end
+    
+    it "will generate a new id for each reservation" do 
+      expect(@reservation.reservation_id).must_equal 1
     end
   end 
 end
