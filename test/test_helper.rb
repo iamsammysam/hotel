d1d31f41d5a5df03bf82c0c8b05371dfa3eb0111
Samantha Coll <samcoll@Samanthas-MacBook-Pro.local>
2019-09-07 13:08:41 -0700

    requested relatives and simplecov.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,16 @@
-# Add simplecov
+require "simplecov"
+SimpleCov.start
+
 require "minitest"
 require "minitest/autorun"
 require "minitest/reporters"
 
+require "date"
+
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
-# require_relative your lib files here!
+# require_relative your lib files here
+require_relative "../lib/hotel_organizer"
+require_relative "../lib/reservation"
+require_relative "../lib/room"
+
