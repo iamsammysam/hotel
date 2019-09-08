3b33a010e36898a4cc00a82a89b9eb00fa3dea8d
Samantha Coll <samcoll@Samanthas-MacBook-Pro.local>
2019-09-08 13:22:46 -0700

    created add_reservation, room_status and tests

--- lib/room.rb
+++ lib/room.rb
@@ -1,3 +1,5 @@
+require "pry"
+
 module Hotel
   class Room
     attr_reader :room_number, :room_cost, :reservations
@@ -7,10 +9,24 @@ module Hotel
       @room_cost = room_cost
       @reservations = reservations
     end
-  end
-end 
-# def add_reservation(reservation)
-#   @reservations << reservation
-# end 
-
-
+    
+    def add_reservation(reservation)
+      @reservations << reservation
+    end 
+    
+    def room_status(start_date, end_date)
+      room_booked_dates = []
+      
+      @reservations.each do |reservation|
+        duration = (reservation.end_date - reservation_start_date).to_i
+        
+        duration.times do |i|
+          date = reservation.start_date + i
+          room_booked_dates << date
+        end
+      end
+      
+      return room_booked_dates
+    end 
+  end 
+end
