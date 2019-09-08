de5937986d246d951f27a56545046fabe6b6aa78
Samantha Coll <samcoll@Samanthas-MacBook-Pro.local>
2019-09-08 16:49:44 -0700

    refactoring

--- lib/room.rb
+++ lib/room.rb
@@ -1,32 +1,10 @@
-require "pry"
-
 module Hotel
   class Room
-    attr_reader :room_number, :room_cost, :reservations
+    attr_reader :room_number, :room_cost
     
     def initialize(room_number, room_cost = 200)
       @room_number = room_number
       @room_cost = room_cost
-      @reservations = reservations
     end
-    
-    def add_reservation(reservation)
-      @reservations << reservation
-    end 
-    
-    def room_status(start_date, end_date)
-      room_booked_dates = []
-      
-      @reservations.each do |reservation|
-        duration = (reservation.end_date - reservation_start_date).to_i
-        
-        duration.times do |i|
-          date = reservation.start_date + i
-          room_booked_dates << date
-        end
-      end
-      
-      return room_booked_dates
-    end 
   end 
 end
--- test/room_test.rb
+++ test/room_test.rb
@@ -1,32 +1,20 @@
 require_relative 'test_helper'
 
 describe "Room Class" do
+  before do
+    @room = Hotel::Room.new(18, 200)
+  end
+  
   describe "initialize" do
     it "is an instance of Room" do 
-      @room = Hotel::Room.new(18, 200)
       expect(@room).must_be_kind_of Hotel::Room
     end 
     
     it "checks data structures at initialize" do
-      @room = Hotel::Room.new(18, 200)
       expect(@room.room_number).must_be_kind_of Integer
       expect(@room.room_cost).must_be_kind_of Integer
       #expect(@room.reservations).must_be_kind_of Array
     end
   end
-  
-  # describe "method add_reservation" do
-  # end 
-  
-  describe "method room_status" do
-    it "returns room status for a date range" do
-      room = Hotel::Room.new(18, 200)
-      reservation = Hotel::Reservation.new(1, room, Date.new(2018,01,01), Date.new(2018,01,06))
-      
-      #expect(room.room_status(Date.new(2018,01,01), Date.new(2018,01,06))).must_equal true
-      
-      room.add_reservation(reservation)
-      expect(room.room_status(Date.new(2018,01,01), Date.new(2018,01,06))).must_equal Array
-    end
-  end
 end
+
